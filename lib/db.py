6b2ae0c75a4294247a0e0017b6fb8477a7b98d87
karanja03 <wambuik03@gmail.com>
2023-09-06 15:05:37 +0300

    Added join tables

--- lib/db.py
+++ lib/db.py
@@ -24,12 +24,12 @@ class Ingredient(Base):
 
     id= Column(Integer, primary_key=True)
     name= Column(String)
-    quantity= Column(String)
-    units= Column(String)
+    # quantity= Column(String)
+    # units= Column(String)
     recipeId= Column(Integer , ForeignKey('recipes.id'))
 
     def __repr__(self):
-        return f"Ingredient(id={self.id!r}, name={self.name}, quantity={self.quantity}, units= {self.units}, recipeId={self.recipeId})"
+        return f"Ingredient(id={self.id!r}, name={self.name}, recipeId={self.recipeId})"
     
 
 class StepTable(Base):
@@ -37,11 +37,11 @@ class StepTable(Base):
 
     id= Column(Integer, primary_key=True)
     description= Column(String)
-    steporder= Column(Integer)
+    # steporder= Column(Integer)
     recipeId= Column(Integer , ForeignKey('recipes.id'))
 
     def __repr__(self):
-        return f"StepTable(id={self.id!r}, description={self.description},  steporder= {self.steporder}, recipeId={self.recipeId})"
+        return f"StepTable(id={self.id!r}, description={self.description}, recipeId={self.recipeId})"
     
 
 class Category(Base):
@@ -53,5 +53,57 @@ class Category(Base):
 
     def __repr__(self):
         return f"Category(id={self.id!r}, name={self.name}, recipeId={self.recipeId})"
+    
+
+# join tables
+
+class RecipeIngredient(Base):
+    __tablename__ = 'recipe_ingredient'
+
+    id = Column(Integer, primary_key=True)
+    recipe_id = Column(Integer, ForeignKey('recipes.id'))
+    ingredient_id = Column(Integer, ForeignKey('ingredients.id'))
+    quantity = Column(String)
+    units = Column(String)
+
+    recipe = relationship('Recipe', back_populates='ingredients')
+    ingredient = relationship('Ingredient', back_populates='recipes')
+
+    def __repr__(self):
+        return f"RecipeIngredient(id={self.id!r}, recipe_id={self.recipe_id!r}, ingredient_id={self.ingredient_id!r}, quantity={self.quantity!r}, units={self.units!r})"
+    
+class RecipeStep(Base):
+    __tablename__ = 'recipe_step'
+
+    id = Column(Integer, primary_key=True)
+    recipe_id = Column(Integer, ForeignKey('recipes.id'))
+    step_id = Column(Integer, ForeignKey('stepstofollow.id'))
+    step_order = Column(Integer)
+
+    recipe = relationship('Recipe', back_populates='steps')
+    step = relationship('StepTable', back_populates='recipes')
+
+    def __repr__(self):
+        return f"RecipeStep(id={self.id!r}, recipe_id={self.recipe_id!r}, step_id={self.step_id!r}, step_order={self.step_order!r})"
+    
+
+
+class RecipeCategory(Base):
+    __tablename__ = 'recipe_category'
+
+    id = Column(Integer, primary_key=True)
+    recipe_id = Column(Integer, ForeignKey('recipes.id'))
+    category_id = Column(Integer, ForeignKey('foodcategories.id'))
+
+    recipe = relationship('Recipe', back_populates='categories')
+    category = relationship('Category', back_populates='recipes')
+
+    def __repr__(self):
+        return f"RecipeCategory(id={self.id!r}, recipe_id={self.recipe_id!r}, category_id={self.category_id!r})"
+
+
+
+
+
 
 
